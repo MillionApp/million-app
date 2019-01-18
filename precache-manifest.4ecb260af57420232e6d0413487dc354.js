e8415c01e53a1c172218edae8dc409c1668fa4fa
João Gustavo de Paula <dev.joaogustavo@gmail.com>
2019-01-18 00:19:23 -0200

    set path correctly

---
+++ precache-manifest.4ecb260af57420232e6d0413487dc354.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1206ccdc79c644fd1d7",
+    "url": "/million-app/css/app.c6e4a9c6.css"
+  },
+  {
+    "revision": "b1206ccdc79c644fd1d7",
+    "url": "/million-app/js/app.5d4a5c51.js"
+  },
+  {
+    "revision": "07e9a2fc047a6931fd07",
+    "url": "/million-app/js/chunk-2d0d63f1.14d61d8e.js"
+  },
+  {
+    "revision": "5de393a0ead52fa1b490",
+    "url": "/million-app/js/chunk-2d208356.0c75f6bf.js"
+  },
+  {
+    "revision": "88cd946c80381136f290",
+    "url": "/million-app/js/chunk-2d211008.a792ddea.js"
+  },
+  {
+    "revision": "423151965537d005f6ad",
+    "url": "/million-app/css/chunk-vendors.46113fd0.css"
+  },
+  {
+    "revision": "423151965537d005f6ad",
+    "url": "/million-app/js/chunk-vendors.29a69a32.js"
+  },
+  {
+    "revision": "92470fe44d0e7b002342c1f30f10925b",
+    "url": "/million-app/img/lighten-logo.92470fe4.svg"
+  },
+  {
+    "revision": "bd9772301daa49d150eb9db3aba36862",
+    "url": "/million-app/img/dark-planning.bd977230.jpg"
+  },
+  {
+    "revision": "373860ceb7c43cc3e653b4c63eaed7f2",
+    "url": "/million-app/img/logo.373860ce.svg"
+  },
+  {
+    "revision": "9b170217e0c85411ab5d432646690e20",
+    "url": "/million-app/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/million-app/robots.txt"
+  }
+];
