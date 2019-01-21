e4ce364250182e5b504a92a9b661c3d27bd076f1
João Gustavo de Paula <dev.joaogustavo@gmail.com>
2019-01-21 18:19:39 -0200

    Wip XV

--- dist/precache-manifest.829cb3e951c305706e62e02cb6f46682.js
+++ dist/precache-manifest.829cb3e951c305706e62e02cb6f46682.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "5add4f15fe80cad317b4",
+    "revision": "a5b9e04eeeb09e0407c4",
     "url": "/million-app/css/app.c1a72137.css"
   },
   {
-    "revision": "5add4f15fe80cad317b4",
-    "url": "/million-app/js/app.d0af96af.js"
+    "revision": "a5b9e04eeeb09e0407c4",
+    "url": "/million-app/js/app.1217bfac.js"
   },
   {
-    "revision": "921dbaac80ce12bdff6a",
-    "url": "/million-app/css/chunk-vendors.5d12c258.css"
+    "revision": "201008dea4fe46eb6651",
+    "url": "/million-app/css/chunk-vendors.f9957ef6.css"
   },
   {
-    "revision": "921dbaac80ce12bdff6a",
-    "url": "/million-app/js/chunk-vendors.3131d9d8.js"
+    "revision": "201008dea4fe46eb6651",
+    "url": "/million-app/js/chunk-vendors.5ca3af6e.js"
   },
   {
     "revision": "92470fe44d0e7b002342c1f30f10925b",
@@ -28,15 +28,15 @@ self.__precacheManifest = [
     "url": "/million-app/img/logo.373860ce.svg"
   },
   {
-    "revision": "b3fa120d89aa03b8be4e64267e7bc88c",
+    "revision": "02dd4e3192d472f14bf9a7dee0a9af9c",
     "url": "/million-app/index.html"
-  },
-  {
-    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "/million-app/robots.txt"
   },
   {
     "revision": "1cd01ed4110aff04641e749c08fe5b17",
     "url": "/million-app/404.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/million-app/robots.txt"
   }
 ];
